00b218845af784a973c5d1cdec40f1d1e4cedc9b
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-06-05 19:14:00 +0200

    anchor.ts aktualisieren

--- soldoge-staking-ui/public/src/components/lib/anchor.ts
+++ soldoge-staking-ui/public/src/components/lib/anchor.ts
@@ -1,1 +1,14 @@
+import { Connection, PublicKey } from "@solana/web3.js";
+import { AnchorProvider, Program, Idl } from "@project-serum/anchor";
+import idl from "./soldoge_idl.json"; // Copy from `target/idl/soldoge_staking.json`
 
+export const PROGRAM_ID = new PublicKey("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkgEoYy3z2i5A");
+export const connection = new Connection("https://api.devnet.solana.com");
+
+export function getProvider(wallet: any) {
+  return new AnchorProvider(connection, wallet, { commitment: "confirmed" });
+}
+
+export function getProgram(wallet: any) {
+  return new Program(idl as Idl, PROGRAM_ID, getProvider(wallet));
+}
