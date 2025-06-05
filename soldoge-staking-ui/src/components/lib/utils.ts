1d05066c63952d7e78dfc6beafde8f8a59c8fa0e
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-06-05 18:43:17 +0200

    utils.ts aktualisieren

--- soldoge-staking-ui/src/components/lib/utils.ts
+++ soldoge-staking-ui/src/components/lib/utils.ts
@@ -1,1 +1,28 @@
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddress,
+  createAssociatedTokenAccountInstruction,
+} from "@solana/spl-token";
+import { PublicKey, Transaction } from "@solana/web3.js";
 
+export async function getOrCreateATA({
+  owner,
+  mint,
+  payer,
+}: {
+  owner: PublicKey;
+  mint: PublicKey;
+  payer: PublicKey;
+}) {
+  const ata = await getAssociatedTokenAddress(mint, owner);
+  const ix = createAssociatedTokenAccountInstruction(
+    payer,
+    ata,
+    owner,
+    mint,
+    TOKEN_PROGRAM_ID,
+    ASSOCIATED_TOKEN_PROGRAM_ID
+  );
+  return { ata, ix };
+}
