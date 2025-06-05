50ce6bb931708d44a906a01cdc2d0fda2bc74c2e
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-06-05 19:14:56 +0200

    ConnectWallet.tsx aktualisieren

--- soldoge-staking-ui/public/src/components/ConnectWallet.tsx
+++ soldoge-staking-ui/public/src/components/ConnectWallet.tsx
@@ -1,1 +1,9 @@
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
+export default function ConnectWallet() {
+  return (
+    <div className="mb-4">
+      <WalletMultiButton />
+    </div>
+  );
+}
