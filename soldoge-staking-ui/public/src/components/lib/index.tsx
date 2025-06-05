20eaa2f01a65966cd0ba7329cbf0c1d826bb49b5
GitCodeXxX <paymentservicequickpay@gmail.com>
2025-06-05 19:13:43 +0200

    index.tsx aktualisieren

--- soldoge-staking-ui/public/src/components/lib/index.tsx
+++ soldoge-staking-ui/public/src/components/lib/index.tsx
@@ -1,1 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./main.css";
 
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+const wallets = [new PhantomWalletAdapter()];
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+  <ConnectionProvider endpoint="https://api.devnet.solana.com">
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
+);
